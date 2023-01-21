458d2083d597ae295163c6544fe02b397a24ce6c
marcuszcoding <marcuszcoding@gmail.com>
2023-01-21 13:38:41 -0700

    counter now turns red if past the character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-  
+  $("#tweet-text").on("input", function(){
+    let currentLength = $(this).val().length
+    $(".counter").text(140-currentLength)
+    if (currentLength > 140) {
+      $(".counter").css("color", "red")
+    } else {
+      $(".counter").css("color", "initial")
+    }
+  });
 });
 
-const textArea = document.querySelector('#tweet-text');
+// 1.) Ducment.ready accessing the file
+// 2.) access the tweet text area and have it so on input we change the counter 
+//     with a function
+// 3.) 
