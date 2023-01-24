b244825a797ee292b0b3d03d75ab62ef0affa2fb
marcuszcoding <marcuszcoding@gmail.com>
2023-01-23 17:47:56 -0700

    adding form submission via jQuery and fetching tweets via jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,30 +10,30 @@
 
 //User Objects
 // Fake data taken from initial-tweets.json
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 $(document).ready(function() {
   const createTweetElement = function(tweet) {
@@ -41,7 +41,7 @@ $(document).ready(function() {
   <article class="tweet">
   <header class="tweet-header"> 
       <div>
-        <i class="fa-solid fa-user"></i>
+        <img src=${tweet.user.avatars}>
         ${tweet.user.name}
       </div>
       <span>
@@ -52,7 +52,7 @@ $(document).ready(function() {
     <textarea>${tweet.content.text}</textarea>
   </div>
   <footer class="tweet-footer">
-    <span>${tweet.created_at}</span>
+    <span>${jQuery.timeago(tweet.created_at)}</span>
     <div>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-sharp fa-solid fa-retweet"></i>
@@ -63,19 +63,27 @@ $(document).ready(function() {
 }
 
 const renderTweets = function(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (let tweet of tweetData) {
+  for (let tweet of tweets) {
     $('#tweets-container').append(createTweetElement(tweet));
   }
 }
 
-renderTweets(tweetData)
+const loadTweets = function() {
+  $.ajax("/tweets", { method: 'GET'})
+  .then(function(data){
+    console.log(data)
+    renderTweets(data)
+  })
+}
 
-// $('#tweets-container').append($tweet);
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// console.log($('#tweets-container'))
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+loadTweets()
+
+// renderTweets(tweetData)
+
+const $form = $('#tweet-form');
+$form.on('submit', function (event) { 
+  event.preventDefault()
+  const data = $('#tweet-text').serialize();
+  $.ajax("/tweets", { method: 'POST', data})
+  })
 })
